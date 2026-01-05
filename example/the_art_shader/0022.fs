99f6f3d8b5e8f05af084dae890bd3d3a5af93d65
fantasialin <fantasialin@gmail.com>
2026-01-05 11:44:05 +0800

    Update 0022.fs
    update sdf circle function

--- example/the_art_shader/0022.fs
+++ example/the_art_shader/0022.fs
@@ -12,17 +12,32 @@ uniform vec4 iMouse;        // xy: current (if MLB down), zw: click
 precision mediump float;
 #endif
 
+// Signed distance function for a circle
+vec3 sdfCircle(vec2 uv, float r) {
+    float x = uv.x;
+    float y = uv.y;
+
+    float d = length(vec2(x, y)) - r;
+
+    return d > 0. ? vec3(1.) : vec3(0., 0., 1.);
+}
+
+// Anti-aliased circle
 float circle(in vec2 uv, in float _radius){
-    vec2 dist = uv-vec2(0.5);
 	return 1.-smoothstep(_radius-(_radius*0.01),
                          _radius+(_radius*0.01),
-                         dot(dist,dist)*4.0);
+                         dot(uv,uv)*4.0);
 }
 
 void main(){
 	vec2 uv = gl_FragCoord.xy/iResolution.xy;
-
-	vec3 color = vec3(circle(uv,0.9));
+	uv -= 0.5; // <-0.5, 0.5>
+	//uv.x *= iResolution.x/iResolution.y; // fix aspect ratio
+	#if 1
+        vec3 color = vec3(circle(uv,0.9));
+    #else
+		vec3 color = sdfCircle(uv, 0.45); 
+	#endif
 
 	gl_FragColor = vec4( color, 1.0 );
 }
